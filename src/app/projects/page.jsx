346fc64b157a041291d9e807b7ed6a9ee1cf9d58
Pranjul Khankriyal <shivamkhankriyal515@gmail.com>
2026-03-03 12:50:14 +0530

    add my projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Star from "../component/star/star";
+import Link from "next/link";
 import style from "./projects.module.css";
 import { Big_Shoulders } from "next/font/google";
 
@@ -12,20 +13,48 @@ const bigShoulders = Big_Shoulders({
 export default function ProjectsPage() {
   return (
     <div className={style.wrapper}>
-      
+
       <h1 className={`${style.title} ${bigShoulders.className}`}>
         PROJECTS
       </h1>
 
       <div className={style.star}>
-        <Star/>
+        <Star />
       </div>
 
-      <div className={style.projectsGrid}>
-        <div className={style.projectCard}></div>
-        <div className={style.projectCard}></div>
-        <div className={style.projectCard}></div>
-        <div className={style.projectCard}></div>
+      <div className={`${style.projectsGrid} ${bigShoulders.className}`}>
+
+        <Link href="https://edu-tech-rouge.vercel.app/" target="_blank">
+          <div className={style.projectCard}>
+            <div className={style.projectContainer} style={{ backgroundImage: "url('/edutech.png')" }}>
+            </div>
+            <div className={style.projectTitle}>EduTech</div>
+          </div>
+        </Link>
+
+        <Link href="https://github.com/Pranjul2002/Letter-Level-Sign-Recognition" target="_blank">
+          <div className={style.projectCard}>
+            <div className={style.projectContainer} style={{ backgroundImage: "url('/Sign.png')", backgroundColor: "#0c0c0c" }}>
+            </div>
+            <div className={style.projectTitle}> Letter Level Sign Laguage detection</div>
+          </div>
+        </Link>
+
+        <Link href="https://github.com/Pranjul2002/StockMarketPricePridiction-backend" target="_blank">
+          <div className={style.projectCard}>
+            <div className={style.projectContainer} style={{ backgroundImage: "url('/stock.jpg')" }}>
+            </div>
+            <div className={style.projectTitle}>Stock market Price Prediction</div>
+          </div>
+        </Link>
+
+        <Link href="https://github.com/Pranjul2002/StockMarketPricePridiction-backend" target="_blank">
+          <div className={style.projectCard}>
+            <div className={style.projectContainer} style={{ backgroundImage: "url('/stock.jpg')" }}>
+            </div>
+            <div className={style.projectTitle}>Stock market Price Prediction</div>
+          </div>
+        </Link>
       </div>
 
     </div>
